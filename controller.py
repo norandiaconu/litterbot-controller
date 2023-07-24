660be1e2110279da899c6c9c609d568bc2866250
Noran Diaconu <norandiaconu@gmail.com>
2023-07-24 16:54:51 -0400

    Initial commit

---
+++ controller.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import asyncio
+from pylitterbot import Account
+from datetime import date
+
+username = ""
+password = ""
+
+async def main():
+  print("Logging in...")
+  dateStr = str(date.today())
+  account = Account()
+  try:
+    await account.connect(username=username, password=password, load_robots=True)
+    for robot in account.robots:
+      print("Robot:", robot.name, "("+robot.model+")")
+      print("Current status:", robot.status)
+      history = await robot.get_activity_history()
+      eventsToday = 0
+      for event in history:
+        eventStr = str(event)
+        if eventStr.startswith(dateStr):
+          eventsToday += 1
+      print("Events today:", eventsToday)
+      next = input("""
+      Enter one of the following numbers to continue or anything else to disconnect:
+      1) Cycle litterbot
+      2) View history
+      3) View insight
+      """)
+      if next == "1":
+        await robot.start_cleaning()
+        print("Started cycle...")
+      elif next == "2":
+        for event in history:
+          print(event)
+      elif next == "3":
+        insight = await robot.get_insight()
+        print(insight)
+  finally:
+    print("Disconnecting...")
+    await account.disconnect()
+
+if __name__ == "__main__":
+  asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+  asyncio.run(main())
